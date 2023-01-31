5a1c5af65a61bbedeaa2d41e3fc9a5c77c6134be
lili11.yang <lili11.yang@tcl.com>
2023-01-31 14:58:03 +0800

    feat:init

---
+++ .umirc.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'umi';
+
+export default defineConfig({
+  nodeModulesTransform: {
+    type: 'none',
+  },
+  routes: [
+    { path: '/', component: '@/pages/index' },
+  ],
+  layout: {}, // 采用antd-design-pro布局
+  fastRefresh: {},
+});
